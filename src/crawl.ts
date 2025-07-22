8714080a2e265e10bde67dde013a6f30b24d6b35
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2025-07-22 10:22:06 -0400

    feat: implement async crawler

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,4 +1,5 @@
 import { JSDOM } from "jsdom";
+import pLimit from "p-limit";
 
 export const normalizeURL = (urlString: string): string => {
   let normalizedURL = urlString.toLowerCase();
@@ -31,4 +32,93 @@ export const getURLsFromHTML = (html: string, baseURL: string): string[] => {
     }
   }
   return urls;
+};
+
+class ConcurrentCrawler {
+  baseURL: string;
+  pages: Record<string, number>;
+  limit: ReturnType<typeof pLimit>;
+
+  constructor(
+    baseURL: string,
+    pages: Record<string, number>,
+    limit: ReturnType<typeof pLimit>
+  ) {
+    this.baseURL = baseURL;
+    this.pages = pages;
+    this.limit = limit;
+  }
+
+  private addPageVisit(normalizedURL: string): boolean {
+    if (this.pages[normalizedURL] == undefined) {
+      this.pages[normalizedURL] = 1;
+      return true;
+    }
+    this.pages[normalizedURL]++;
+    return false;
+  }
+
+  private getHTML = async (url: string): Promise<string> => {
+    const response = await fetch(url);
+    if (response.status > 399) {
+      console.log(`Error fetching ${url}: ${response.statusText}`);
+      return "";
+    }
+
+    const contentType = response.headers.get("content-type");
+    if (!contentType || !contentType.includes("text/html")) {
+      console.log(`Error fetching ${url}: ${response.statusText}`);
+      return "";
+    }
+
+    return await this.limit(async () => {
+      return await response.text();
+    });
+  };
+
+  private crawlPage = async (currentURL: string): Promise<void> => {
+    const baseURLObj = new URL(this.baseURL);
+    const currentURLObj = new URL(currentURL);
+
+    if (currentURLObj.hostname !== baseURLObj.hostname) {
+      return;
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL);
+    const isNewPage = this.addPageVisit(normalizedCurrentURL);
+    if (!isNewPage) {
+      return;
+    }
+
+    console.log(`actively crawling ${currentURL}`);
+
+    try {
+      const htmlBody = await this.getHTML(currentURL);
+
+      if (htmlBody === "") {
+        return;
+      }
+
+      const nextURLs = getURLsFromHTML(htmlBody, this.baseURL);
+
+      await Promise.all(nextURLs.map((nextURL) => this.crawlPage(nextURL)));
+    } catch (err) {
+      console.log(`error in crawlPage: ${err}`);
+    }
+  };
+
+  crawl = async (): Promise<Record<string, number>> => {
+    await this.crawlPage(this.baseURL);
+    return this.pages;
+  };
+}
+
+export const crawlSiteAsync = async (
+  baseURL: string,
+  pages: Record<string, number>,
+  limit: ReturnType<typeof pLimit>
+) => {
+  const crawler = new ConcurrentCrawler(baseURL, pages, limit);
+  const crawledPages = await crawler.crawl();
+  return crawledPages;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+import { argv } from "process";
+import { crawlSiteAsync } from "./crawl";
+import pLimit from "p-limit";
+
+const main = async () => {
+  if (argv.length != 3) {
+    console.log("Usage: npm run start <url>");
+    process.exit(1);
+  }
+
+  const baseURL = argv[2];
+  console.log(`Starting crawl of ${baseURL}`);
+  await crawlSiteAsync(baseURL, {}, pLimit(10));
+};
+
+main();
